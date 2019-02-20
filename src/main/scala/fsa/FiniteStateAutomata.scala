0fe0d3c7d18f8fa3b79b04fd0ac75402cc939560
gregoryamitten@gmail.com <gregoryamitten@gmail.com>
2019-02-20 10:50:04 +0000

    further code clean up

--- src/main/scala/fsa/FiniteStateAutomata.scala
+++ src/main/scala/fsa/FiniteStateAutomata.scala
@@ -1,22 +1,21 @@
 package fsa
 
-object FiniteStateAutomata  {
+object FiniteStateAutomata {
   //fsa for Regex 3(45)*5
-  def doesAccept(table: Array[Int]):Boolean = {
+  def doesAccept(table: Array[Int]): Boolean = {
     //init = 0, accept = 1, terminal = 2
     var currentState = 0
 
-    for (v <- table)
-      currentState match {
-        case 0 => if (v != 3) return false else currentState = 1
-        case 1 => if (v == 5) currentState = 2 else if (v != 4) return false
-        case 2 => if (v == 4) currentState = 1 else if (v != 5) return false
-      }
+    for (v <- table) currentState match {
+      case 0 => if (v == 3) currentState = 1 else return false
+      case 1 => if (v == 5) currentState = 2 else if (v != 4) return false
+      case 2 => if (v == 4) currentState = 1 else if (v != 5) return false
+    }
 
     currentState == 2
   }
 
-    def main(args: Array[String]): Unit = {
-      print(doesAccept(Array(3,4,4,5,4,5,5,5)))
-    }
+  def main(args: Array[String]): Unit = {
+    print(doesAccept(Array(3, 4, 4, 5, 4, 5, 5, 5)))
+  }
 }
